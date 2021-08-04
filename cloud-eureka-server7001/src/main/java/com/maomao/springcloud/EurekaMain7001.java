6d7c7f07984f103ea3e6032ffe8f7686c1f3f62a
maohj <2350005497@qq.com>
2021-08-04 23:14:29 +0800

    Eureka单机版配置完成。

--- cloud-eureka-server7001/src/main/java/com/maomao/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/maomao/springcloud/EurekaMain7001.java
@@ -2,18 +2,18 @@ package com.maomao.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * description: OrderMain80
- * date: 2021/8/3 11:19
+ * description: EurekaMain7001
+ * date: 2021/8/4 21:55
  * author: MSXQ
  * version: 1.0
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
